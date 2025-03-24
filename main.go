a2d6eea775c11bac7291bda900075701d2a5be81
Daniel Steman <daniel-steman@live.nl>
2025-03-24 21:25:22 +0100

    read databricks.yml

--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 
 	"cuelang.org/go/cue/cuecontext"
 	"cuelang.org/go/cue/load"
+	"gopkg.in/yaml.v3"
 )
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func resolveIncludes() {
+	file, err := os.Open("databricks.yml")
+	check(err)
+	defer file.Close()
+	data, err := io.ReadAll(file)
+	check(err)
+
+	m := make(map[string]interface{})
+	err = yaml.Unmarshal(data, &m)
+	check(err)
+}
 
 func main() {
 	ctx := cuecontext.New()
